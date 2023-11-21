0d7cf250f129341d70a2e0120a895d51cb212fb2
Naveen-Kumar <30574883+navein-kumar@users.noreply.github.com>
2023-11-21 11:08:49 +0530

    Add files via upload

---
+++ get_session_token.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+curl -X GET \
+-H 'Content-Type: application/json' \
+-H "Authorization: user_token sXkSqfRSXRslxCBRL7xPhhN9XxOKeXxbizigK1d4" \
+-H "App-Token: gymXH2Ha4yFqx4lUG86776u1Ex65TnQvAcijbIu3" \
+'http://127.0.0.1:8080/apirest.php/initSession'
